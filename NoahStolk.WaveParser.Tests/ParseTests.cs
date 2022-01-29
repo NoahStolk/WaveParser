e3a0ea5fce7875c7b6859da61dffb66a45b6f839
Noah Stolk <noah.stolk@gmail.com>
2022-01-29 12:32:30 +0100

    Add empty test project

---
+++ NoahStolk.WaveParser.Tests/ParseTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NoahStolk.WaveParser.Tests;
+
+[TestClass]
+public class ParseTests
+{
+	[TestMethod]
+	public void ParseWaveBytes()
+	{
+	}
+
+	[TestMethod]
+	public void ParseWaveStream()
+	{
+	}
+
+	[TestMethod]
+	public void ParseWaveFile()
+	{
+	}
+}
